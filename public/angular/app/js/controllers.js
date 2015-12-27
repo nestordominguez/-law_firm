9870efbaccdc4604fac320c4a6296d689b0ffe4d
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2015-12-27 00:45:03 -0300

    finish service for pages

--- public/angular/app/js/controllers.js
+++ public/angular/app/js/controllers.js
@@ -3,8 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('appController', ['$scope', 'pagesService', function($scope, pagesService) {
+    pagesService.async().then(function(data) {
+      $scope.links = data;
+    });
   }])
   .controller('MyCtrl2', [function() {
 
--- public/angular/app/js/services.js
+++ public/angular/app/js/services.js
@@ -6,4 +6,23 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1')
+  .factory('pagesService', ["$http", function($http) {
+    var pagesService = {
+      async: function() {
+        var promise = $http({
+          method: 'GET',
+          url: '/api/v1/pages'
+        }).then(function(response) {
+          var links = [];
+          for (var i = response.data.length - 1; i >= 0; i--) {
+            links.push(response.data[i].link) ;
+          };
+          return links;
+        });
+        return promise;
+        }
+      }
+
+    return pagesService;
+  }]);
