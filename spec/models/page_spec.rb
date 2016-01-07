d17d867bfc572e239ac7b70e0b32f0126085c8ef
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2016-01-06 21:55:40 -0300

    fix spec model page

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -3,65 +3,76 @@ require 'database_cleaner'
 
 describe Page do
 
-  context "should not be saved" do
+  context "when not be saved" do
     it "without any info" do
       page = Page.create
       expect(Page.all).to be_empty
     end
+
     it "without link" do
       page = build(:page, link: "").save
       expect(Page.all).to be_empty
     end
+
     it "without content" do
       page = build(:page, content: "").save
       expect(Page.all).to be_empty
     end
+
     it "without title" do
       page = build(:page, title: "").save
       expect(Page.all).to be_empty
     end
+
     it "without priority" do
       page = build(:page, priority: "").save
       expect(Page.all).to be_empty
     end
+
     it "with a link that exist" do
       page = create(:page)
       page_not_saved = build(:page, priority: 2).save
       expect(Page.all.count).to eq(1)
     end
-    it "with a priority lower than 1" do
+
+    it "with a priority 0" do
       page = build(:page, priority: 0).save
       expect(Page.all).to be_empty
     end
+
     it "with a priority higher than the cuantity of rows plus 1" do
       create(:page)
       page = build(:page, link: "link1", priority: 3).save
       expect(Page.all.count).to eq(1)
     end
-    it "if there are higher than 15 rows in db" do
+
+    it "there are higher than 15 rows in db" do
       16.times {|n| build(:page, link: "#{n}", priority: "#{n +1}").save}
       expect(Page.all.count).to eq(15)
     end
   end
 
-  context "should be saved" do
+  context "when be saved" do
     it "with all params" do
       expect(Page.all).to be_empty
       create(:page)
       expect(Page.all).not_to be_empty
     end
+
     it "with a priority in range and not is in db" do
       create(:page)
       build(:page, link: "link1", priority: 2).save
       expect(Page.all.count).to eq(2)
     end
-    it "with a priority in range and is in db" do
+
+    it "with a priority that is in db" do
       create(:page)
       build(:page, link: "link1").save
       expect(Page.find(1).priority).to eq(2)
       expect(Page.find(2).priority).to eq(1)
     end
-    it "if there are less than 15 rows" do
+
+    it "there are less than 15 rows" do
       15.times {|n| build(:page, link: "#{n}", priority: "#{n +1}").save}
       expect(Page.all.count).to eq(15)
     end
@@ -69,23 +80,20 @@ describe Page do
   end
 
   context "whould not update" do
+
     before {create(:page)}
-    it "if priority are lower than 1" do
+
+    it "priority are lower than 1" do
       page = Page.find(1)
       page.priority = 0
       expect(page.save).to be false
     end
-    it "if priority are out of de range" do
-      build(:page, link: "link1", priority: 2).save
+
+    it "priority are haigher of the range" do
       page = Page.find(1)
-      page.priority = 3
+      page.priority = 2
       page.save
       expect(Page.find(1).priority).to eq(1)
-      expect(Page.find(2).priority).to eq(2)
-      page.priority = 0
-      page.save
-      expect(Page.find(1).priority).to eq(1)
-      expect(Page.find(2).priority).to eq(2)
     end
   end
 
