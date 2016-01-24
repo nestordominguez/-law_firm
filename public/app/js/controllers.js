24f6d242cba882f13bf432f5e97b9a5559b5b401
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2016-01-24 20:19:49 -0300

    finished http service in service

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -3,27 +3,33 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('appController', ['$scope', '$routeParams', 'pagesService',
-    function($scope, $routeParams, pagesService) {
-      pagesService.then(function(response) {
+  controller('appController', ['$scope', '$routeParams', 'pageService',
+    function($scope, $routeParams, pageService) {
+
+      pageService.show("").then(function(response) {
+
         var links = [];
           for (var i = response.data.length - 1; i >= 0; i--) {
             links.push(response.data[i]);
           };
+
         $scope.links = links;
         $scope.page_link = $routeParams.page_link;
+
         $scope.select = function(link) {
           return link === $routeParams.page_link ? 'active' : '';
         }
       });
+
   }])
-  .controller('showController', ['$scope', '$http', '$routeParams',
-    function($scope, $http, $routeParams) {
-      $http({
-        method: 'GET',
-        url: 'http://localhost:3000/api/v1/pages/' + $routeParams.page_link
-      }).then(function(response) {
+  .controller('showController', ['$scope', '$routeParams', 'pageService',
+    function($scope, $routeParams, pageService) {
+
+      pageService.show("/" + $routeParams.page_link)
+      .then(function(response) {
+
         $scope.title = response.data.title;
         $scope.content = response.data.content;
+
       })
   }]);
--- public/app/js/services.js
+++ public/app/js/services.js
@@ -2,31 +2,14 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('myApp.services', [])
-  .service('pagesService', ["$http", function($http) {
-    /*var pagesService = {
-      async: function() {
-        var promise = $http({
+  .service('pageService', ["$http", function ($http) {
+    return {
+      show: function(link) {
+        return $http({
           method: 'GET',
-          url: 'http://localhost:3000/api/v1/pages'
-        }).then(function(response) {
-          var links = [];
-          for (var i = response.data.length - 1; i >= 0; i--) {
-            links.push(response.data[i]) ;
-          };
-          return links;
-        });
-        return promise;
-        }
+          url: 'http://localhost:3000/api/v1/pages' + link
+        })
       }
-    return pagesService;*/
-
-    var promise = $http({
-      method: 'GET',
-      url: 'http://localhost:3000/api/v1/pages'
-    });
-    return promise;
+    }
   }]);
