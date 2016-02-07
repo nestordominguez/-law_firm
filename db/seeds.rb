ab06b2e834474a2b017a5b5d908118cc612cd788
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2016-02-07 00:12:51 -0300

    add lorem gem and add to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'rubygems'
+require 'lorem'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,6 +9,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 links = %w[estudio links publicaciones staff areas]
+content = Lorem::Base.new('paragraphs', 5).output
 links.each_with_index do |link, index|
-  Page.new(link: link, title: link, content: link, priority: index + 1).save
+  Page.new(link: link, title: link, content: content, priority: index + 1).save
 end
